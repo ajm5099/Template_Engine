c03ac0e669f31415a746057846b2f980a30eea8b
Alex Milroy <amilroy@gmail.com>
2020-10-05 18:29:29 -0700

    all tests are now working

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,12 +1,12 @@
-const Employee = require("./Employee")
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
 
 class Engineer extends Employee {
     //in the constructor, include everything you are asking for, including what you are importing with super
-    constructor (name, id, email, gitHubUser) {
+    constructor (name, id, email, github) {
         //this is taking the name, ID and email directly from the employee class, and placing them here. You dont need to as kfor these again, because they are being transported here using super. However, you need to include them in the constructor.
         super (name, id, email);
-        this.gitHubUser = gitHubUser;
+        this.github = github;
     }
 
     getRole() {
@@ -14,7 +14,7 @@ class Engineer extends Employee {
     }
 
     getGithub() {
-        return this.gitHubUser;
+        return this.github;
     }
 }
 
